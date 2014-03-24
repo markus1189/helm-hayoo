9c4cd8f5db069a4abf0a222962319e8ab0464dbf
Markus Hauck <markus1189@gmail.com>
2014-03-24 19:13:04 +0100

    Example usage in commentary

--- helm-hayoo.el
+++ helm-hayoo.el
@@ -26,6 +26,11 @@
 ;; This package provides a helm source `helm-source-hayoo' and a
 ;; configured helm `helm-hayoo' to search hayoo online.
 ;;
+;; Example queries to test (enter text displayed after '> '):
+;;
+;; > lens traversed
+;; > simpleHttp
+;; > modifySTRef
 
 ;;; Code:
 
